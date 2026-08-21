a34a7dad828454ff14020c00a55de02a29230eaa
Daniel Beeke <daniel.beeke@triply.cc>
2026-08-21 15:47:15 +0200

    swapping trix for tip top

--- packages/shacl-everything/src/widgets/implementations/shui/editors/BooleanEditor/widget.tsx
+++ packages/shacl-everything/src/widgets/implementations/shui/editors/BooleanEditor/widget.tsx
@@ -1,5 +1,14 @@
+import { factory } from "@/helpers/factory.ts";
+import { xsd } from "@/helpers/namespaces.ts";
 import type { WidgetProps } from "@/widgets/types.ts";
 
-export default function BooleanEditor({ shape }: WidgetProps) {
-  return <input type="checkbox" />;
+export default function BooleanEditor({ term, setTerm, labelledBy }: WidgetProps) {
+  return (
+    <input
+      type="checkbox"
+      checked={term.value === "true"}
+      onChange={(e) => setTerm(factory.literal(String(e.target.checked), xsd("boolean")))}
+      aria-labelledby={labelledBy}
+    />
+  );
 }
--- packages/shacl-everything/src/widgets/implementations/shui/editors/DateTimePickerEditor/widget.tsx
+++ packages/shacl-everything/src/widgets/implementations/shui/editors/DateTimePickerEditor/widget.tsx
@@ -1,5 +1,6 @@
+import TextFieldEditor from "@/widgets/implementations/shui/editors/TextFieldEditor/widget.tsx";
 import type { WidgetProps } from "@/widgets/types.ts";
 
-export default function DateTimePickerEditor({ shape }: WidgetProps) {
-  return <input type="datetime-local" />;
+export default function DateTimePickerEditor(props: WidgetProps) {
+  return <TextFieldEditor type="datetime-local" {...props} />;
 }
