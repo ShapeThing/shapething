ef9c5f3a1f1c2539caa17825e0318192e8269ab9
Daniel Beeke <daniel.beeke@triply.cc>
2026-08-26 08:29:17 +0200

    fix for storybook

--- packages/shacl-everything/.storybook/addons/submit-preview/constants.ts
+++ packages/shacl-everything/.storybook/addons/submit-preview/constants.ts
@@ -5,6 +5,9 @@ export const SUBMIT_PREVIEW_EVENT = `${SUBMIT_PREVIEW_ADDON_ID}/update`;
 export type SubmitPreviewPayload = {
   storyId: string;
   dataGraph: string;
+  // Undefined when the story has no focusNode - resource-only scoping needs one to know what to
+  // fetch a description of.
+  resourceOnly: string | undefined;
   additions: string;
   deletions: string;
 };
